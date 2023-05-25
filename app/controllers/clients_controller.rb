408512f4f2d7de34b6b05a727969e67b9c8637c6
Yogeshvari2902 <yogeshvari2022@gmail.com>
2023-05-25 17:43:38 +0530

    Ruby MAILER_Practice_codeUpdate

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -36,29 +36,6 @@ class ClientsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /clients/1 or /clients/1.json
-  def update
-    # respond_to do |format|
-    #   if @client.update(client_params)
-    #     format.html { redirect_to client_url(@client), notice: "Client was successfully updated." }
-    #     format.json { render :show, status: :ok, location: @client }
-    #   else
-    #     format.html { render :edit, status: :unprocessable_entity }
-    #     format.json { render json: @client.errors, status: :unprocessable_entity }
-    #   end
-    # end
-  end
-
-  # DELETE /clients/1 or /clients/1.json
-  def destroy
-    # @client.destroy
-
-    # respond_to do |format|
-    #   format.html { redirect_to clients_url, notice: "Client was successfully destroyed." }
-    #   format.json { head :no_content }
-    # end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_client
@@ -67,7 +44,6 @@ class ClientsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def client_params
-      # debugger
       params.require(:client).permit(:name, :email, :login)
     end
 end
